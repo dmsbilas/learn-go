408742899d8c3f4f84117fe2f04bd0fa00906274
Bilas Siddiq <dmsbilas@yahoo.com>
2021-07-14 13:25:46 +0600

    boolen data type

---
+++ datatype-boolean.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/** Data Type : Boolean */
+
+	var earthIsRound bool = true
+	var earthIsFlat bool = false
+
+	fmt.Println("Eart is flat is ", earthIsFlat, "Statement ")
+
+	fmt.Println("Earth is oval is a ", earthIsRound, "statement")
+
+}
